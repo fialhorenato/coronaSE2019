a6f4a80ce4dffa973f262f5f38c9174ff39a2266
Renato da Silva Reis Fialho <renato@renatofialho.com>
2020-03-20 22:22:08 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import requests
+import json
+from datetime import date
+import time
+import os
+import tweepy
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+def makeTweet(tweet):
+    apiKey = "8AEFNZ1HgrgFWoMgyVXtBl6Ra"
+    secretKey = "QlBVo0VlVYWoTxZn10jWla4Dg0tq2tOYZIxZko9aX1EDAc7vKE"
+    accessToken = "1240332676336410624-rEXprZ5ZJCnsnrYlQSxchYWXdvThxX"
+    tokenSecret = "fSjF6QuKqcD3gv3XNTD5ZELtvqGoMH7WepknMNjTYllDz"
+    
+    auth = tweepy.OAuthHandler(apiKey, secretKey)
+    auth.set_access_token(accessToken, tokenSecret)
+    api = tweepy.API(auth)
+
+    api.update_status(tweet)
+    
+def makeApiRequest():
+    print("Starting job")
+    posted = False
+    while (not posted):
+        response = requests.get("https://coronavirus-19-api.herokuapp.com/countries")
+        try:
+            for object in json.loads(response.content):
+                if object['country'] == 'Sweden':
+                    header = "Corona Virus Cases in Sweden"
+                    data = "📅  Date = %s" % (date.today())
+                    hashtags = "#COVID19 #CoronaSverige #Coronavirus #CoronaSweden"
+                    tweet = " %s \n\n %s \n %s \n %s" % (header , data , formatTwitterEnglish(object), hashtags)
+                    postTweet(tweet)
+
+                    header = "Corona Virus Cases i Sverige"
+                    data = "📅 Datum = %s" % (date.today())
+                    hashtags = "#COVID19 #CoronaSverige #Coronavirus #CoronaSweden"
+                    tweet = " %s \n\n %s \n %s \n %s" % (header , data , formatTwitterSwedish(object), hashtags)
+                    postTweet(tweet)
+                    posted = True
+                    print("Finished job")
+                    print("Finished job")
+        except Exception as identifier:
+            time.sleep(5)
+            print(identifier)
+            posted = True
+    
+    
+    
+
+def formatTwitterEnglish(object):
+    confirmedCases = "🤒 Confirmed Cases = %s" % (object['cases'])
+    criticalCases = "😷 Critical Cases = %s" % (object['critical'])
+    deaths = "😢 Deaths = %s" % (object['deaths'])
+    recovered = "🥳 Recovered = %s" % (object['recovered'])
+    return ('%s \n %s \n %s \n %s \n ' % (confirmedCases, criticalCases, deaths, recovered))
+
+def formatTwitterSwedish(object):
+    confirmedCases = "🤒 Bekräftade fall = %s" % (object['cases'])
+    criticalCases = "😷 Kritiska fall = %s" % (object['critical'])
+    deaths = "😢 Dödsfall = %s" % (object['deaths'])
+    recovered = "🥳 Krya på sig = %s" % (object['recovered'])
+    return ('%s \n %s \n %s \n %s \n' % (confirmedCases, criticalCases, deaths, recovered))
+
+def postTweet(tweet):
+        makeTweet(tweet)
+
+makeApiRequest()
